f827d942cbdaddc897630eb2aff8fbc2d83b65ff
Sebastian Bergmann <sb@sebastian-bergmann.de>
2012-10-05 10:47:35 +0200

    Do not perform string replacement for @package_version@ anymore.

--- Bytekit/Scanner/Rule/ZendView.php
+++ Bytekit/Scanner/Rule/ZendView.php
@@ -49,7 +49,6 @@
  * @author    Lars Strojny <lstrojny@php.net>
  * @copyright 2009-2012 Sebastian Bergmann <sb@sebastian-bergmann.de>
  * @license   http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
- * @version   Release: @package_version@
  * @link      http://github.com/sebastianbergmann/bytekit-cli/tree
  * @since     Class available since Release 1.0.0
  */
